6bbe7661f9210eef58327763def41cd7841ca86f
4lexFro <k43bfg@gmail.com>
2023-12-17 17:49:46 +0300

    create new_project

---
+++ Lection_1/task_1.py
@@ -0,0 +1,19 @@
+name = 'Alex'
+age = None
+
+a = 42
+print(id(a))
+
+a = 'helo world'
+print(id(a))
+
+print(name, age, a, 'text', sep=' (=^.^=) ', end='#')
+print('any text')
+
+res = input('print your text: ')
+print('Ты написал ', res)
+
+age = int(input('Сколько тебе лет? '))
+
+
+
