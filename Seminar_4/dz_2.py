4fbbcfb669930d6156348ec211533e98b2ed2ed8
4lexFro <k43bfg@gmail.com>
2023-12-27 22:04:54 +0300

    dz seminar 4

--- Seminar_4/dz_2.py
+++ Seminar_4/dz_2.py
@@ -36,3 +36,14 @@ b = '',
 c = [],
 d = {})  # (a=1, b='hello', c=[1, 2, 3], d={})
 print(params)
+
+# Решение с сайта:
+#
+# def key_params(**kwargs):
+#     result = {}
+#     for key, value in kwargs.items():
+#         if isinstance(value, (int, str, float, bool, tuple)):
+#             result[value] = key
+#         else:
+#             result[str(value)] = key
+#     return result
